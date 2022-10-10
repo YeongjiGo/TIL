26e2fc78be3ae1e675369417a9287a6f6bcf19a5
YeongjiGo <eyokenda105@gmail.com>
2022-10-10 14:35:29 +0900

    ㄴ

--- backjoon/gg.py
+++ backjoon/gg.py
@@ -1,7 +1,7 @@
 def func(n):
     remain = 1000-n
     li = [500, 100, 50, 10, 5, 1]
-    ans = 0 #동전개수
+    ans = 0 #동전개수수
     for i in range(len(li)):
         ans += remain // li[i]
         remain = remain % li[i]
