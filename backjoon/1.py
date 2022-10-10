4a6e805e5a63e7dd20cb86f6b7e46293297b4b30
YeongjiGo <eyokenda105@gmail.com>
2022-10-10 15:12:18 +0900

    2022.10.10 backjoon 11047번 동전0 / 그리디알고리즘

--- backjoon/1.py
+++ backjoon/1.py
@@ -1,12 +1,1 @@
-def func(n):
-    remain = 1000-n
-    li = [500, 100, 50, 10, 5, 1]
-    ans = 0 #동전개수
-    for i in range(len(li)):
-        ans += remain // li[i]
-        remain = remain % li[i]
-    return ans
-
-
-n = int(input())
-print(func(n))
+print(4200%50000)
--- backjoon/11047.py
+++ backjoon/11047.py
@@ -1,1 +1,18 @@
 n, k  = map(int,input().split())
+coin = 0 
+L = []
+for i in range(n):
+    coin = int(input())
+    L.append(coin)
+    
+L.sort(reverse = True)
+
+cnt = 0 #동전개수
+i  = 0 #리스트 인덱스
+remain = k
+
+for i in range(len(L)):
+    cnt += remain // L[i]
+    remain = remain % L[i]
+print(cnt)
+
