cbc429e3802a0e08a00fad9935d8b6524fc2d0a7
YeongjiGo <eyokenda105@gmail.com>
2022-10-21 14:41:23 +0900

    selection sort/ 2022.10.21 PM 2:41

---
+++ backjoon/selection_sort.py
@@ -0,0 +1,15 @@
+arr = [4, 2, 3, 9, 5, 7]
+index, temp = 0, 0
+for i in range(5):
+    min = 999
+    for j in range(i, 5):
+        if min > arr[j]:
+            min = arr[j]
+            index = j
+    temp = arr[i]
+    arr[i] = arr[index]
+    arr[index] = temp
+
+
+for i in arr:
+    print(i, end = ' ' )
