5e32e5bb371b30d14dc2b990cf5b7c8d23890125
YeongjiGo <eyokenda105@gmail.com>
2022-10-25 16:53:06 +0900

    backjoon 23881 selection sort

---
+++ backjoon/23881.py
@@ -0,0 +1,22 @@
+##5 2
+##3 1 2 5 4
+##0 1 2 3 4
+
+n,k = map(int,input().split())
+arr = list(map(int,input().split()))
+check  =0 
+tmp, index = 0, 0
+
+for i in range(n-1,-1, -1):
+    max = -100
+    for j in range(0, i+1):
+        if max < arr[j]:
+            max = arr[j]
+            index = j
+
+    tmp = arr[i]
+    arr[i] = arr[index]
+    arr[index] = tmp
+    print(arr)
+
+
