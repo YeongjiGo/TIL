f5625c5416e96c5420cbbcb2b11152d3dafd6d90
YeongjiGo <eyokenda105@gmail.com>
2022-11-04 12:37:12 +0900

    study on 221104 PM 12:37

--- backjoon/23881.py
+++ backjoon/23881.py
@@ -4,19 +4,25 @@
 
 n,k = map(int,input().split())
 arr = list(map(int,input().split()))
-check  =0 
+
+cnt = 0
 tmp, index = 0, 0
 
 for i in range(n-1,-1, -1):
     max = -100
+    
     for j in range(0, i+1):
         if max < arr[j]:
             max = arr[j]
             index = j
-
+        
     tmp = arr[i]
     arr[i] = arr[index]
     arr[index] = tmp
-    print(arr)
+    for i in range(n):
+        testarr[i] = arr[i]
 
+    for i in range(n):
 
+    if cnt == k:
+        print(arr)
